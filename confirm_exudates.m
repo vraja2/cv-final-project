74f67f6c1c705cf43f3d1bfd2e3ba414156eec0c
Vignesh Raja <vignesh@vigneshraja.com>
2015-05-07 20:19:16 -0500

    confirm_exudates returns pixels within percentiles ranges

--- confirm_exudates.m
+++ confirm_exudates.m
@@ -1,7 +1,7 @@
-function confirm_exudates(im,exudates,x,y,radius)
+function selected_pixels = confirm_exudates(im,exudates,x,y,radius)
     [h,w,~] = size(im);
     [cols, rows] = meshgrid(1:w, 1:h);
-    get_pixels_disk(x,y,radius,rows,cols)
+    get_pixels_disk(x,y,radius,rows,cols);
     for i=1:size(exudates,2)
         curr_exudate = exudates{i};
         [ys, xs] = ind2sub([h,w], curr_exudate);
@@ -11,7 +11,7 @@ function confirm_exudates(im,exudates,x,y,radius)
         for j=1:length(ys)
             curr_pixel = im(ys(j),xs(j),:);
             red_intensities = [red_intensities curr_pixel(1)];
-            green_intensities = [green_intensitirs curr_pixel(2)];
+            green_intensities = [green_intensities curr_pixel(2)];
         end
         I_green_low = prctile(green_intensities,5,2);
         I_red_low = prctile(red_intensities,5,2);
@@ -28,13 +28,24 @@ function confirm_exudates(im,exudates,x,y,radius)
         X = X(:);
         Y = Y(:);
         neighborhood = [];
-        points = [X Y];
+        points = [Y X];
         for j=1:size(points,1)
             if ~ismember(points(j,:),[ys xs],'rows')
                 %unique neighbors
                 neighborhood = vertcat(neighborhood,points(j,:));
             end
         end
+        %go through each neighbor and check whether it's RG components like
+        %within the range
+        selected_pixels = [];
+        for j=1:size(neighborhood,1)
+            pixel_components = im(neighborhood(j,1),neighborhood(j,2),:);
+            r_val = pixel_components(1);
+            g_val = pixel_components(2);
+            if g_val >= I_green_low && g_val <= I_green_high && r_val >= I_red_low && r_val <= I_red_high
+                selected_pixels = vertcat(selected_pixels,neighborhood(j,:));
+            end
+        end 
     end
 end
 
